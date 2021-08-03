dd96fceb229ba19a4de7b4038f392feff3b35605
Mark Francisco <mfrancisco581@gmail.com>
2021-08-03 01:32:44 -0500

    save changes for the night

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-# organizations = Organization.create([:name => "workforce.com", :hourly_rate => 40],[:name => "pringles", :hourly_rate => 25])
+organizations = Organization.create([:name => "workforce.com", :hourly_rate => 40],[:name => "pringles", :hourly_rate => 25])
 
-# users = User.create([
-#   { :organization_id => organizations.first.id, :name => "rafael nadal" :email_address => "rafa@example.com", :password_digest => "password" },
-#   { :name => "alison wonderland", :email_address => "alison@example.com", :password_digest => "password" },
-# ])
+users = User.create([
+  { :organization_id => organizations.first.id, :name => "rafael nadal" :email_address => "rafa@example.com", :password_digest => "password" },
+  { :name => "alison wonderland", :email_address => "alison@example.com", :password_digest => "password" },
+])
 
-# shift1 = Shift.create(:user_id => users.first.id, :start => Time.zone.parse('2021-08-01 10:00'), :finish => Time.zone.parse('2021-08-01 6:00'), :break_length => 60)
+shift1 = Shift.create(:user_id => users.first.id, :start => Time.zone.parse('2021-08-01 10:00'), :finish => Time.zone.parse('2021-08-01 18:00'), :break_length => 60)
