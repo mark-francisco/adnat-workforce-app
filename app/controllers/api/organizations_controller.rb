f5241811f15a86ff0fcfbca21d05fad3d550af67
Mark Francisco <mfrancisco581@gmail.com>
2021-08-03 17:52:14 -0500

    finish Organization routes/views

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -1,4 +1,43 @@
 class Api::OrganizationsController < ApplicationController
   def index
+    @organizations = Organization.all
+    render "index.json.jb"
+  end
+
+  def show
+    @organization = Organization.find_by(:id => params[:id])
+    render "show.json.jb"
+  end
+
+  def create
+    @organization = Organization.new(
+      :name => params[:name],
+      :hourly_rate => params[:hourly_rate],
+    )
+    if @organization.save
+      render "show.json.jb"
+    else
+      render :json => { :errors => @organization.errors.full_messages }, :status => :bad_request
+    end
+  end
+
+  def update
+    @organization = Organization.find_by(:id => params[:id])
+
+    @organization.name = params[:name] || @organization.name
+    @organization.hourly_rate = params[:hourly_rate] || @organization.hourly_rate
+
+    if @organization.save
+      render "show.json.jb"
+    else
+      render :json => { :errors => @organization.errors.full_messages }, :status => :bad_request
+    end
+  end
+
+  def destroy
+    @organization = Organization.find_by(:id => params[:id])
+
+    @organization.destroy
+    render :json => { :message => "Organization was destroyed." }
   end
 end
