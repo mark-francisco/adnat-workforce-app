0bc10d7c8c8c5336fb0e57ac1ca30c530129e476
Mark Francisco <mfrancisco581@gmail.com>
2021-08-04 19:12:04 -0500

    add current_user to views, update cascading delete for organization and user models

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,6 +2,6 @@ class Organization < ApplicationRecord
   validates :name, :presence => true
   validates :hourly_rate, :presence => true, :numericality => true
 
-  has_many :users
-  has_many :shifts, :through => :users
+  has_many :users, :dependent => :nullify
+  has_many :shifts, :through => :users, :dependent => :destroy
 end
