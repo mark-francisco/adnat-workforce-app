16d0050930ca8eae3453768fb6e0c875acd9c7c5
Mark Francisco <mfrancisco581@gmail.com>
2021-08-05 00:19:04 -0500

    finish model methods for calculated fields. expose these add'l fields in view

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,6 +2,30 @@ class Shift < ApplicationRecord
   validates :user_id, :presence => true, :numericality => { :only_integer => true }
   validates :start, :presence => true
   validates :finish, :presence => true
+  validate :finish_after_start
 
   belongs_to :user
+
+  def finish_after_start
+    if self.finish < self.start
+      errors.add(:finish, "must be after start")
+    end
+  end
+
+  def shift_date
+    return self.start.to_date
+  end
+
+  def hours_worked
+    total_shift_hours = TimeDifference.between(self.start, self.finish).in_hours
+    break_hours = self.break_length / 60
+
+    return total_shift_hours - break_hours
+  end
+
+  def shift_cost
+    organization = Organization.find_by(:id => self.user.organization_id)
+
+    return self.hours_worked * organization.hourly_rate
+  end
 end
